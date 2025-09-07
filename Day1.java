7e43c5ec3e7f4a4eff65a86c8b955a7d8757e5bd
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-07 14:50:37 +0800

    contoh 1

--- Day1.java
+++
@@ -1,5 +0,0 @@
-public classs day1{
-  public static void main(String[] args{
-      System.out.println("hallo world");
-}
-}
---
+++ contoh 1.java
@@ -0,0 +1,5 @@
+public classs main {
+  public static void main(String[] args{
+      System.out.println("Hello");
+}
+}
