6afd3dc89fd2f51aa9807280f00a49d877da089d
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-08 19:01:38 +0800

    HelloWorld

--- Day1.java
+++ Day1.java
@@ -1,7 +1,7 @@
 package elmutt;
 public class elsaaaaa {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hello world");
     }
     
 }
