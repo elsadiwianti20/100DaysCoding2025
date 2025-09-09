a0126bdda5f02ddc161ac1866544e3aa4d387eef
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-09 18:35:48 +0700

    Printl(),Print(),Printf()

---
+++ Day2.java
@@ -0,0 +1,10 @@
+package day2;
+public class codingday2 {
+    public static void main(String[] args) {
+        System.out.println("elsyaa");
+        System.out.print(18);
+        System.out.printf("nama:%s, umur:%d,","elsyaa", 18);
+        
+    }
+    
+}
