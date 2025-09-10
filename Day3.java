24843ece2a58d2193d374f32552b5d0970b0f8ca
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-10 19:04:36 +0700

    memahami dan menggunaka komentar

---
+++ Day3.java
@@ -0,0 +1,24 @@
+package CodingDay3;
+/**
+ *
+ * @author elsaaa
+ * program sederhana untuk menampilkan teks ke layar
+ */
+public class day3 {
+    public static void main(String[] args) {
+        //komentar untuk satu baris//
+        /*Komentar lebih dari satu baris*/
+        
+        //program untuk menampilkan nama hari//
+        /*Baris di bawah ini akan menampilkan nama hari ke layar*/
+        System.out.println("Hari ini adalah Rabu");
+        
+        //program dimulai dari method main//
+        /*Baris di bawah ini akan menampilkan hobi ke layar*/
+        System.out.println("Saya suka menyanyi tapi suara saya jelek");
+       
+        
+        
+    }
+    
+}
