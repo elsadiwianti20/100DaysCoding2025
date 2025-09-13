59242672bedc909de0f986b32e4304f66f3a9afa
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-13 18:57:29 +0700

    TipeDataPecahan/Desimal(double,float)

---
+++ Day6.java
@@ -0,0 +1,14 @@
+package day6.java;
+public class codingday6 {
+    public static void main(String[] args) {
+        
+        double uangRafiahmad = 100.00000000000;
+        System.out.println("uangRafiahmad : " + uangRafiahmad );
+        
+        float tinggiBadan = 155.5f;
+        System.out.println("Tinggi badan : " + tinggiBadan + "cm");
+          
+        
+    }
+    
+}
