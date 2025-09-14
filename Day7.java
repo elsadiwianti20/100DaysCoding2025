f1140f0de0f066372b26173df9bbcbc3fa483bea
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-14 20:04:03 +0800

    TipeDataKarakter(Char,Boolean)

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package Day7.java;
+public class codingday7 {
+    public static void main (String[] args) {
+        //contoh penggunaan char
+        char huruf = 'E';
+        System.out.println("huruf"+huruf);
+
+        //contoh penggunaan boolean,true = benar, false =salah
+        boolean sudahMakan = true;
+        System.out.println("Sudah makan?"+sudahMakan);
+
+        boolean belumMakan = false;
+        System.out.println("Belum makan?"+belumMakan);
+    }
+}
+
+  
