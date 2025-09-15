6ca67eaa6e5e31bd284a83347cbe348c9a320b8a
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-15 20:57:14 +0800

    penggunaanStringPadaTeks

--- Day8.java
+++ Day8.java
@@ -1,16 +1,11 @@
 package Day8.java;
 public class Codingday8 {
      public static void main (String[] args) {
-          String nama = "Elsa_diwianti";
-          String makananFavorit = "semuaMakanan";
-          String bias = "Jungkook";
+          String namaPanjangPakeSekali= ("namaku Elsaaaa Pake diwiantiiiii");
+          system.out.println("namaPanjangPakeSekali:"+namaPanjangPakeSekali);
 
-          System.out.println("Nama:"+nama);
-          System.out.println("Makanan favorit:"+ makananFavorit);
-          System.out.println("Bias:"+bias);
-
-          String teks = "Halo, namaku " + nama+".Aku suka " +makananFavorit + " dan aku menyukai "+ bias +".";
-
-          System.out.println(teks);
+          String alamat= ("Jl.poros majene : dusun 1 Botto ";
+          system.out.println ("alamat:"+alamat);
+          
     }
 }
