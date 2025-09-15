459b2cde06a794106a3d51e698c12bbf17567ff4
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-15 20:29:14 +0800

    pengenalanTipeDataStringUntukTeks

---
+++ Day8.java
@@ -0,0 +1,16 @@
+package Day8.java;
+public class Codingday8 {
+     public static void main (String[] args) {
+          String nama = "Elsa_diwianti";
+          String makananFavorit = "semuaMakanan";
+          String bias = "Jungkook";
+
+          System.out.println("Nama:"+nama);
+          System.out.println("Makanan favorit:"+ makananFavorit);
+          System.out.println("Bias:"+bias);
+
+          String teks = "Halo, namaku " + nama+".Aku suka " +makananFavorit + " dan aku menyukai "+ bias +".";
+
+          System.out.println(teks);
+    }
+}
