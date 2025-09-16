6cad53fd30ee53f0ad59216526c2f6f269e7cd5e
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-16 20:17:45 +0800

    Deklarasi,InsialisasiDanUpdateVariabel

---
+++ Day9.java
@@ -0,0 +1,21 @@
+package Day9.java;
+public class Codingday9 {
+    public static void main (String[] args) {
+        String sekolah;
+        int umur;
+      
+        sekolah = "SMANCA";
+        umur = 17;
+
+        System.out.println("Sekolah:"+ sekolah );
+        System.out.println("Umur:"+ umur );
+
+
+        sekolah = "UNSULBAR";
+        umur = 18;
+          
+        System.out.println("Sekolah baru:"+ sekolah );
+        System.out.println("Umur baru:"+ umur );
+
+    }
+}
