2ba1b7a1eb6d48dd201870f48856a9efe6dd3558
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-17 19:43:48 +0700

    Constanta(finalVariabel)

---
+++ Day10.java
@@ -0,0 +1,29 @@
+package Day10.java;
+public class Codingday10 {
+    public static void main(String[] args) {
+      
+        String nama = "Elsa";
+        byte umur = 18;
+        short tahunLahir = 2006;
+        char golonganDarah = 'B';
+        double tinggiBadan = 155.5;
+        boolean masihSekolah = false;
+        boolean sudahKuliah = true;
+        int angkaFavorit = 20;
+        float beratBadan = 52.5f;
+        long angka = 12345678910L;
+        
+        System.out.println("Nama:"+ nama);
+        System.out.println("Umur:"+ umur);
+        System.out.println("Tahun lahir:"+ tahunLahir);
+        System.out.println("Golongan darah:"+ golonganDarah);
+        System.out.println("Tinggi badan:"+ tinggiBadan);
+        System.out.println("Masih sekolah:"+ masihSekolah);
+        System.out.println("Sudah kuliah:"+ sudahKuliah);
+        System.out.println("Angka favorit:"+ angkaFavorit);
+        System.out.println("Berat badan:"+ beratBadan);
+        System.out.println("Angka:"+ angka);
+        
+    }
+   
+}
