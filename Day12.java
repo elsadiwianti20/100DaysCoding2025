7bcdc6e749b301c5f0551942115adfa2582b5e10
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-20 19:23:21 +0700

    MembuatProgramBiodataSederhana

--- Day12.java
+++ Day12.java
@@ -5,7 +5,7 @@ public class Codingday12 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Biodata");
+        System.out.println(" input Biodata");
         
         System.out.println("nama");
         String nama = input.nextLine();
@@ -38,8 +38,22 @@ public class Codingday12 {
         System.out.println("Golongan darah");
         char golonganDarah = input.next().charAt (0);
         
+        System.out.println("Biodata");
+        System.out.println("Nama:"+ nama);
+        System.out.println("Umur:"+ umur);
+        System.out.println("Jumlah saudara:"+ jumlahBersaudara);
+        System.out.println("Tahun lahir:"+ tahunLahir);
+        System.out.println("No HP:"+ noHp);
+        System.out.println("Tinggi:"+tinggi);
+        System.out.println("Ukuran sepatu:"+ ukuranSepatu);
+        System.out.println("Perempuan:"+ perempuan);
+        System.out.println("Laki-laki:"+ laki_laki);
+        System.out.println("Golongan darah:"+ golonganDarah);
+        
         
           
     }
     
 }
+
+       
