4a5e4b7cf245e85708c5702c49d276236d1cd0c5
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-21 21:01:40 +0700

    MengubahStringMenjadiPrimitif

---
+++ Day14.java
@@ -0,0 +1,39 @@
+package Day14;
+public class Codingday14 {
+    public static void main(String[] args) {
+        String a = "18";
+        String b = "2006";
+        String c = "20";
+        String d = "12345678910";
+        String e = "155.5";
+        String f = "50.5";
+        String g = "B";
+        String h = "true";
+        
+        
+        //String ka int
+        byte umur = Byte.parseByte(a);
+        short tahunLahir = Short.parseShort(b);
+        int angkaFavorit =Integer.parseInt(c);
+        long angka1_10 = Long.parseLong(d);
+        float tinggi = Float.parseFloat(e);
+        double bb = Double.parseDouble(f);
+        char golonganDarah = g.charAt(0);
+        boolean pintar = Boolean.parseBoolean(h);
+        
+        System.out.println("String " + a +" jadi byte " + umur);
+        System.out.println("String " + b +" jadi short " + tahunLahir);
+        System.out.println("String " + c +" jadi int " + angkaFavorit);
+        System.out.println("String " + d +" jadi long " + angka1_10);
+        System.out.println("String " + e +" jadi float " + tinggi);
+        System.out.println("String " + f +" jadi double " + bb);
+        System.out.println("String " + g +" jadi char "+ golonganDarah);
+        System.out.println("String " + h +" jadi boolean " + pintar);
+        
+        
+        
+        
+        
+    }
+    
+}
