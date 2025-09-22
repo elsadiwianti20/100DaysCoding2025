e5e4f0feab6cb56f401900b78066f50c32cdaeda
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-22 21:59:20 +0800

    operatorAritmetika(+),(-)

---
+++ Day15.java
@@ -0,0 +1,15 @@
+package Day15.java;
+public class Codingday15 {
+   public static void main (String [] args ) {
+      int a = 2;
+      int b = 3;
+
+      //penjumlahan 
+      int tambah = a + b ;
+      System.out.println("Hasil:" + tambah );
+
+      //penguraian 
+      int kurang = a - b ;
+      System.out.println("Hasil:" + kurang );
+   }
+}
