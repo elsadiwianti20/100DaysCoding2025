d30860b9bd50dca3534eac476acd6726865b96e3
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-23 21:15:08 +0800

    operatorAritmetika(+),(-)

--- Day16.java
+++ Day16.java
@@ -1,16 +1,16 @@
 package Day16.java;
 public class codingday16 {
     public static void main(String[] args) {
-       
-        //operator aritmetika perkalian
-         int a = 3;
-        int b = 5;
-        int hasilPerkalian = a*b;
-        System.out.println("Hasil perkalian:" + hasilPerkalian);
-        
-        //operator aritmetika pembagian
+    
         int c = 6;
         int d = 2;
+
+        //operator aritmetika perkalian
+        int hasilPerkalian = c*d;
+        System.out.println("Hasil perkalian:" + hasilPerkalian);
+        
+        
+        //operator aritmetika pembagian
         int hasilPembagian = c/d;
         System.out.println("Hasil pembagian :" + hasilPembagian);
     }
