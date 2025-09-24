2fa440df763871575e050943ba8f6aba3a2e6eef
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-24 19:55:19 +0700

    operatorAritnetika(%)atauSisaBagi

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package Day17.java;
+public class codingday17 {
+    public static void main(String[] args) {
+        
+        int e = 20;
+        int f = 6;
+        
+        //Operator aritmetika modulus atau sisa bagi
+        int hasil = e % f;
+        System.out.println(" Sisa dari " + e + " % " + f + " = " + hasil);
+    }
+    
+}
