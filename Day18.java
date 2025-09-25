3401d57ebc45f03ac3899ab4f9ef6c3737660ff8
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-25 20:25:11 +0700

    otomatisTipeDataKecilKeBesar

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package Day18.java;
+public class codingday18 {
+    public static void main(String[] args) {
+        
+        byte kecil = 10;
+        int besar = kecil; 
+        //otomatis byte ke int
+        System.out.println("byte: " + kecil);
+        System.out.println("int: " + besar);
+    }
+    
+}
