8271a7477259acd195360524c2d5e9ea3cb95028
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-26 19:51:47 +0700

    KonversiManual/Paksa(dariTipeDataBesarKeKecil)

---
+++ Day19.java
@@ -0,0 +1,11 @@
+package Day19.java;
+public class codingday19 {
+    public static void main(String[] args) {
+        double desimal = 20.0;
+        int bulat = (int)desimal ;
+        // konversi manual/paksa(dari tipe data besar ke kecil)
+        System.out.println("Double: "+ desimal);
+        System.out.println("Int: " + bulat);
+    }
+    
+}
