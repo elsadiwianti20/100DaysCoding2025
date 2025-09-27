8adf441ad65177c73e3431cc6113feb867dd927e
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-09-27 19:49:32 +0700

    mengubahTipeDataPrimitifMenjadiString

---
+++ Day20.java
@@ -0,0 +1,26 @@
+package Day20.java;
+public class codingday20 {
+    public static void main(String[] args) {
+        byte umur = 18;
+        short tahun_kelahiran = 2006;
+        int tinggi_badan = 155;
+        long nomorHp = 82395879964L;
+        float bb = 55.5f;
+        boolean perempuan = true;
+        char jk = 'P';
+        double harga_minyak = 15.000;
+        
+        
+        System.out.println("byte ke String:" + umur);
+        System.out.println("short ke String:" + tahun_kelahiran);
+        System.out.println("Integer ke String:" + tinggi_badan);
+        System.out.println("long ke String:" + nomorHp);
+        System.out.println("float ke String:" + bb);
+        System.out.println("boolean ke String:" + perempuan);
+        System.out.println("char ke String:" + jk);
+        System.out.println("double ke String:" + harga_minyak);
+        
+           
+    }
+    
+}
