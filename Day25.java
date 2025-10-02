d258ebb95435cd2a3d759a3f61e70a5279d7fd61
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-02 21:40:01 +0700

    OperatorIncrementdanDecrememnt(++,--)

---
+++ Day25.java
@@ -0,0 +1,24 @@
+package Day25.java;
+public class codingday25 {
+    public static void main(String[] args) {
+        
+        byte a = 2;
+        System.out.println("nilai  awal = " + a );
+        
+        //increment
+        System.out.println("a++ = " + (a++));
+        
+        System.out.println("hasil increment =" + a);
+      
+      
+        //Decrement 
+        byte b = 5;
+        System.out.println("b-- = " + (b--));
+        System.out.println("hasil decrement = " + b);
+       
+       
+        
+        
+    }
+    
+}
