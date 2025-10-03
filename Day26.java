9a44ebb0a7ed023ebe624517b8f9dda85873d570
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-03 20:46:35 +0700

    operatorPenugasan (=, +=, -=, dll)

---
+++ Day26.java
@@ -0,0 +1,19 @@
+package Day26.java;
+public class codingday26 {
+    public static void main(String[] args) {
+        
+        int y = 8;
+        System.out.println("Nilai awal y = " + y);
+        y += 4;
+        System.out.println("Setelah y += 4, nilai y = " + y);
+        y -= 3;
+        System.out.println("Setelah y -= 3, nilai y = " + y);
+        y *= 5;
+        System.out.println("Setelah y *= 5, nilai y =" + y);
+        y %= 6;
+        System.out.println("Setelah y %= 6, nilai y =" + y);
+        
+        
+    }
+    
+}
