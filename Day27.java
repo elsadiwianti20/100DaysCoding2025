beee6d8e79e75c4835bbd82d6163702519b55a9b
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-04 21:03:09 +0800

    EvaluasiKe2

---
+++ Day27.java
@@ -0,0 +1,19 @@
+package Day27;
+
+import java.util.Scanner;
+
+public class Codingday27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai a:");
+        int a = input.nextInt();
+
+        System.out.println("Masukkan nilai b:");
+        int b = input.nextInt();
+
+        System.out.println("Hasil output:");
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
