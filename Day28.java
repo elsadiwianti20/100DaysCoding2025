d31ac22a5e07005bbe8fd520f842f1ca3e5a389c
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-05 18:40:14 +0700

    OperatorPerbandingan(==Dan!=)

---
+++ Day28.java
@@ -0,0 +1,24 @@
+package Day28.java;
+
+import java.util.Scanner;
+
+public class codingday28 {
+    public static void main(String[] args) {
+        
+    Scanner input = new Scanner(System.in);
+    System.out.println("Masukkan angka pertama: " );
+    int a = input.nextInt();
+    
+    System.out.println("Masukkan angka kedua: ");
+    int b = input.nextInt();
+    
+    // Menggunakan operator perbandingan == dan !=
+    System.out.println("===HASIL PERBANDINGAN===");
+    System.out.println("Apakah a == b ? " + (a == b));
+    System.out.println("Apakah a != b ? " + (a != b ));
+    }
+    
+    
+    
+    
+}
