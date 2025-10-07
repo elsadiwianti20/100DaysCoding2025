207a9ca86b6a1571b3a17d01563850c3f48082fa
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-07 20:34:31 +0700

    OperatorPerbandingan(<=Dan>=)

---
+++ Day30.java
@@ -0,0 +1,13 @@
+package Day30.java;
+
+public class Codingday30 {
+    public static void main(String[] args) {
+        int e = 2;
+        int l = 4;
+        
+        System.out.println("e <= l : " + (e <= l)) ;
+        System.out.println("e >= l : " + (e >= l));
+        
+    }
+    
+}
