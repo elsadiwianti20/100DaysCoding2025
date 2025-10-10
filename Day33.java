d51170a2a698c2211920ed14062e856023fb7b3f
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-10 22:17:24 +0800

    OperatorLogika(Not)

---
+++ Day33.java
@@ -0,0 +1,15 @@
+package day33;
+
+public class OperatorLogikaNot {
+    public static void main(String[] args) {
+
+        boolean elsaBingung = true;
+
+        if (!elsaBingung) {
+            System.out.println("Elsa paham pelajarannya");
+        } else {
+            System.out.println("Elsa sedang bingung");
+        }
+
+    }
+}
