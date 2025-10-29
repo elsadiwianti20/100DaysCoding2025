1a2666713f189704966cf80781e335e0ee3f4055
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-29 20:49:51 +0800

    perulangan while

---
+++ Day52.java
@@ -0,0 +1,12 @@
+package elsaaaaa;
+
+public class codingday52 {
+    public static void main(String[] args) {
+        int a = 1; 
+      
+        while (a <= 5) { 
+            System.out.println("Angka ke-" + a);
+            a++; 
+        }
+    }
+}
