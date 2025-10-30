37a7e4d2da55e70c4d9ea2b0a91884a45862756a
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-30 23:00:10 +0800

    keyword break

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package elsaaaa;
+public class codingday53 {
+    public static void main(String[] args) {
+        for (int e = 1; e <= 10; e++) {
+            if (e == 5) {
+                break; 
+            }
+            System.out.println("Angka: " + e);
+        }
+        System.out.println("Perulangan berhenti karena break;");
+    }
+}
