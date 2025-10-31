bee01bb19539e3c47d954e81d7cffd80054b1ce9
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-10-31 19:01:23 +0800

    continue

---
+++ Day54.java
@@ -0,0 +1,12 @@
+package elsaaaa;
+public class codingday54 {
+    public static void main(String[] args) {
+        for (int e = 1; e <= 10; e++) {
+            if (e == 5) {
+                continue; 
+            }
+            System.out.println("Angka: " + e);
+        }
+        System.out.println("Perulangan selesai;");
+    }
+}
