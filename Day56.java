a28c1be1587043914d9554d6a5b56e44c18d8123
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-02 17:50:58 +0800

    do while

---
+++ Day56.java
@@ -0,0 +1,14 @@
+package elsaaaaaa;
+
+public class codingday56 {
+    public static void main(String[] args) {
+        int e = 1;
+
+        do {
+            System.out.println("Angka: " + e);
+            e++;
+        } while (e <= 5);
+
+        System.out.println("Perulangan selesai;");
+    }
+}
