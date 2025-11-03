3fe95d4c3ceeb662d5d1e474c281bebf6926432f
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-04 00:33:49 +0800

    mencetak angka 1N dan N1

---
+++ Day58.java
@@ -0,0 +1,17 @@
+package elsaaaaaa;
+
+public class codingday58 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        System.out.println("Angka 1 sampai N:");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("Angka N sampai 1:");
+        for (int i = N; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
