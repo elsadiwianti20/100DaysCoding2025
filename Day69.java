424afe2f28341277e3976310311d2e8c56178af9
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-06 18:25:19 +0800

    program angka ganjil N-1 dan  angka genap N-1

---
+++ Day69.java
@@ -0,0 +1,21 @@
+package elsaaaaaa;
+
+public class codingday60 {
+    public static void main(String[] args) {
+        int N = 10; 
+
+        System.out.println(" Angka Ganjil dari " + N + " ke 1 ");
+        for (int i = N; i >= 1; i--) { 
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("Angka Genap dari " + N + " ke 1");
+        for (int i = N; i >= 1; i--) { 
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
