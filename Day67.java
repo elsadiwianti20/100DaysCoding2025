722a0fd62a5358e414238fd8e27e778ae8127313
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-13 22:15:47 +0800

    membuat pola horizontal

---
+++ Day67.java
@@ -0,0 +1,16 @@
+package elsaaaaa;
+
+import java.util.Scanner;
+
+public class codingday67_angka {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan batas angka (N): ");
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " "); 
+        }
+    }
+}
