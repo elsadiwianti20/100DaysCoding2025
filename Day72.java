a0293bedc77744f05c60db2916ffe7e3327db87a
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-18 21:27:27 +0800

    membuat pola segitiga siku-siku terbalik

---
+++ Day72.java
@@ -0,0 +1,19 @@
+package elsaaaaa;
+
+import java.util.Scanner;
+
+public class codingday71 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int N = input.nextInt();
+
+        for (int i = N; i >= 1; i--) {        
+            for (int j = 1; j <= i; j++) {    
+                System.out.print("* ");
+            }
+            System.out.println();               
+        }
+    }
+}
