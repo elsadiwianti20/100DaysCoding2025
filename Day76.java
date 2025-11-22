ab7f1fd71c7453bd8246229091f34acb558fef89
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-22 21:53:00 +0800

    method string untuk pengecekan (equals () equallgnoreCase() constains(),isEmpty())

---
+++ Day76.java
@@ -0,0 +1,17 @@
+package elsaaaaa;
+
+public class Day76 {
+    public static void main(String[] args) {
+
+        String kalimat = "Java itu mudah";
+
+        System.out.println(kalimat.equals("Java itu sulit"));
+
+        System.out.println(kalimat.equalsIgnoreCase("java itu mudah"));
+
+        System.out.println(kalimat.contains("mudah"));
+
+        String kosong = "";
+        System.out.println(kosong.isEmpty());
+    }
+}
