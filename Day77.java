c3bf1856135e18cc4c3c104257b9ec872de093fa
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-23 21:54:52 +0800

    method string untuk manipulasi  (substring (), replace () , trim())

---
+++ Day77.java
@@ -0,0 +1,13 @@
+package elsaaaaa;
+public class day77 {
+    public static void main(String[] args) {
+
+        String teks = "  Belajar Java Seru  ";
+
+        System.out.println(teks.substring(2, 9));    
+
+        System.out.println(teks.replace("Java", "Python"));
+
+        System.out.println(teks.trim());
+    }
+}
