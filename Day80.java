8141212c8cef437e3e393adec7b0c2975d4cf7d0
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-26 21:59:42 +0800

    Array 1D

---
+++ Day80.java
@@ -0,0 +1,13 @@
+package elsaaaaa;
+
+public class Day80 {
+    public static void main(String[] args) {
+
+        int [] nilai = {80,68,90,89,99};
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("index " + i + " = " + nilai[i]);
+        }
+
+    }
+}
