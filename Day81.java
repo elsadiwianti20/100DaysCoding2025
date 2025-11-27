f9b77765d23bbb04fd4195226a9a07958250154b
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-11-27 22:16:51 +0800

    mengakses elemen array dengan indeks

---
+++ Day81.java
@@ -0,0 +1,14 @@
+package elsaaaaa;
+
+public class Day81 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Elemen index 0 = " + angka[0]);
+        System.out.println("Elemen index 1 = " + angka[1]);
+        System.out.println("Elemen index 2 = " + angka[2]);
+        System.out.println("Elemen index 3 = " + angka[3]);
+        System.out.println("Elemen index 4 = " + angka[4]);
+    }
+}
