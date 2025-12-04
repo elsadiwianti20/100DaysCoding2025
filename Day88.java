23b90f54b8ded2535241667acb94581bfcab66fd
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-12-04 21:32:15 +0800

    mengakses elemen array menggunakan for each

---
+++ Day88.java
@@ -0,0 +1,14 @@
+package elsaaaaaa;
+
+public class Day88 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Menampilkan elemen array menggunakan for-each:");
+
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}
