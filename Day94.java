cb56f04dece0328ed51edf121ebd7865680c129b
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-12-10 22:07:11 +0800

    mencetak dengan method void

---
+++ Day94.java
@@ -0,0 +1,15 @@
+package elsaaaaaa;
+
+public class Day94 {
+
+    public static void main(String[] args) {
+        pembuka();
+        penutup();
+    }
+    public static void pembuka() {
+        System.out.println("Selamat datang!");
+    }
+    public static void penutup() {
+        System.out.println("Sampai jumpa dan semangat belajar!");
+    }
+}
