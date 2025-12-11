91c4bad94c19c626cb518bdb13628906f9cb1d12
elsadiwianti20 <elsa.diwianti44@sma.belajar.id>
2025-12-11 23:08:12 +0800

    mencetak dengan parameter void

---
+++ Day95.java
@@ -0,0 +1,25 @@
+package elsaaaaaa;
+
+public class Day95 {
+
+    public static void main(String[] args) {
+
+        tanggalLahir("20");
+        bulanLahir("12");
+        tahunLahir(2006);
+        hariLahir("Kamis");
+
+    }
+    public static void tanggalLahir(String tgl) {
+        System.out.println("Tanggal lahir : " + tgl);
+    }
+    public static void bulanLahir(String bln) {
+        System.out.println("Bulan lahir   : " + bln);
+    }
+    public static void tahunLahir(int thn) {
+        System.out.println("Tahun lahir   : " + thn);
+    }
+    public static void hariLahir(String hr) {
+        System.out.println("Hari lahir    : " + hr);
+    }
+}
